518c3bd4512ad51eb630c50c7607ea72a3423596
mbCeli <munbaporicelina@outlook.com>
2025-03-10 10:34:52 +0000

    App.jsx routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
-import HomePage from "./pages/HomePage/HomePage";
-import ProfilePage from "./pages/ProfilePage/ProfilePage";
-import SignupPage from "./pages/SignupPage/SignupPage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-
-import Navbar from "./components/layout/Navbar/Navbar";
 import IsPrivate from "./components/IsPrivate/IsPrivate";
 import IsAnon from "./components/IsAnon/IsAnon";
+
+import HomePage from "./pages/HomePage/HomePage";
 import LandingPage from "./pages/LandingPage/LandingPage";
+import GameManagementPage from "./pages/GameManagementPage/GameManagementPage";
+import GamePage from "./pages/GamePage/GamePage";
+import GameDetailPage from "./pages/GameDetailPage/GameDetailPage";
+import LeaderboardPage from "./pages/LeaderboardPage/LeaderboardPage";
+import SignupPage from "./pages/SignupPage/SignupPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+
+import DashboardPage from "./pages/admin/DashboardPage";
+import GameManagementPage from "./pages/admin/GameManagementPage";
+import UserManagementPage from "./pages/admin/UserManagementPage";
+
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
-
-        <Route path="/Home" element={<HomePage />} />
-
-        <Route
-          path="/profile"
-          element={
-            <IsPrivate>
-              <ProfilePage />
-            </IsPrivate>
-          }
-        />
-
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/games" element={<GamePage />} />
+        <Route path="/games/:id" element={<GameDetailPage />} />
+        <Route path="/leaderboard" element={<LeaderboardPage />} />
+        <Route path="*" element={<NotFoundPage />} />
         <Route
           path="/signup"
           element={
@@ -43,6 +46,38 @@ function App() {
             <IsAnon>
               <LoginPage />
             </IsAnon>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <IsPrivate>
+              <ProfilePage />
+            </IsPrivate>
+          }
+        />
+        <Route
+          path="/admin"
+          element={
+            <IsPrivate adminOnly>
+              <DashboardPage />
+            </IsPrivate>
+          }
+        />
+        <Route
+          path="/admin/games"
+          element={
+            <IsPrivate adminOnly>
+              <GameManagementPage />
+            </IsPrivate>
+          }
+        />
+        <Route
+          path="/admin/users"
+          element={
+            <IsPrivate adminOnly>
+              <UserManagementPage />
+            </IsPrivate>
           }
         />
       </Routes>
